b9b00ffaa3d149fc1be10f21dc1a863487f0bf3f
Claudia Penaloza <claudia.penaloza@conti-na.com>
2025-09-16 14:40:32 -0400

    Logs, next try

--- app.R
+++ app.R
@@ -2,16 +2,26 @@ library(shiny)
 library(glue)
 
 generate_story <- function(noun, verb, adjective, adverb) {
-  glue(
+  story <- glue(
     "
     Once upon a time, there was a {adjective} {noun} who loved to
     {verb} {adverb}. It was the funniest thing ever!
   "
   )
+  
+  # Use message() for reliable console logging
+  message("[LOG] ", Sys.time(), " - Inputs: ",
+          "noun = ", noun,
+          ", verb = ", verb,
+          ", adjective = ", adjective,
+          ", adverb = ", adverb)
+  message("[LOG] ", Sys.time(), " - Generated Story: ", story)
+  
+  return(story)
 }
 
 ui <- fluidPage(
-  titlePanel("Mad Libs Game with Logs"),
+  titlePanel("Mad Libs Game"),
   sidebarLayout(
     sidebarPanel(
       textInput("noun1", "Enter a noun:", ""),
@@ -21,35 +31,14 @@ ui <- fluidPage(
     ),
     mainPanel(
       h3("Your Mad Libs Story:"),
-      textOutput("story"),
-      hr(),
-      h3("Logs:"),
-      verbatimTextOutput("log_output")
+      textOutput("story")
     )
   )
 )
 
 server <- function(input, output) {
-  # reactiveVal to store accumulated log
-  log <- reactiveVal("")
-
   output$story <- renderText({
-    story_text <- generate_story(input$noun1, input$verb, input$adjective, input$adverb)
-
-    # create a log entry
-    new_log <- glue(
-      "[{Sys.time()}] Inputs: noun={input$noun1}, verb={input$verb}, adjective={input$adjective}, adverb={input$adverb}\n",
-      "[{Sys.time()}] Generated Story: {story_text}\n\n"
-    )
-
-    # append the new log to previous log
-    log(paste0(log(), new_log))
-
-    story_text
-  })
-
-  output$log_output <- renderText({
-    log()
+    generate_story(input$noun1, input$verb, input$adjective, input$adverb)
   })
 }
 
