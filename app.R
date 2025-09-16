6297940a44e35c0b2140dc9b7de1e33715454712
Claudia Penaloza <claudia.penaloza@conti-na.com>
2025-09-16 14:30:44 -0400

    Internal log

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(glue)
 
-generate_story <- function(noun, verb, adjective, adverb, log) {
+generate_story <- function(noun, verb, adjective, adverb) {
   story <- glue(
     "
     Once upon a time, there was a {adjective} {noun} who loved to
@@ -9,18 +9,19 @@ generate_story <- function(noun, verb, adjective, adverb, log) {
   "
   )
   
-  # Create log entry
-  log_entry <- glue("[{Sys.time()}] Inputs: noun={noun}, verb={verb}, adjective={adjective}, adverb={adverb}\n",
-                    "[{Sys.time()}] Generated Story: {story}\n")
-  
-  # Append to log
-  log(paste0(log(), log_entry))
+  # Log the inputs and generated story
+  cat("[LOG]", Sys.time(), "- Inputs:",
+      "noun =", noun,
+      ", verb =", verb,
+      ", adjective =", adjective,
+      ", adverb =", adverb, "\n")
+  cat("[LOG]", Sys.time(), "- Generated Story:", story, "\n")
   
   return(story)
 }
 
 ui <- fluidPage(
-  titlePanel("Mad Libs Game with Logs"),
+  titlePanel("Mad Libs Game"),
   sidebarLayout(
     sidebarPanel(
       textInput("noun1", "Enter a noun:", ""),
@@ -30,24 +31,14 @@ ui <- fluidPage(
     ),
     mainPanel(
       h3("Your Mad Libs Story:"),
-      textOutput("story"),
-      hr(),
-      h3("Logs:"),
-      verbatimTextOutput("log_output")
+      textOutput("story")
     )
   )
 )
 
 server <- function(input, output) {
-  # reactiveVal to store log
-  log <- reactiveVal("")
-  
   output$story <- renderText({
-    generate_story(input$noun1, input$verb, input$adjective, input$adverb, log)
-  })
-  
-  output$log_output <- renderText({
-    log()
+    generate_story(input$noun1, input$verb, input$adjective, input$adverb)
   })
 }
 
